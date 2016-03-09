5c2b0416547352d46f6c1d13c325d5a3ffba25c5
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-09 18:55:59 +0000

    Updated test class for contact manager. Now provide mock dependencies.

--- src/test/java/ContactManagerImplTest.java
+++ src/test/java/ContactManagerImplTest.java
@@ -1,56 +1,42 @@
 import interfaces.Contact;
 import interfaces.Meeting;
-import interfaces.PastMeeting;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by Vladimirs Ivanovs on 19/01/16.
  */
 public class ContactManagerImplTest {
     ContactManagerImpl cm;
-    Map<Integer, Meeting> meetings;
+    List<Meeting> meetings;
     Set<Contact> contacts;
-    Meeting mp, mp2, mf, mf2;
-    Calendar dp, dp2, df, df2;
+    Set<Contact> participants;
 
 
     @Before
     public void setUp() throws Exception {
-        cm = new ContactManagerImpl();
-        meetings = new HashMap<>();
+        meetings = new ArrayList<>();
         contacts = new HashSet<>();
-        cm.setMeetings(meetings);
-        cm.setAllContacts(contacts);
 
-        dp = new GregorianCalendar(2014, 1, 1);
-        dp2 = new GregorianCalendar(2015, 1, 1);
-        df = new GregorianCalendar(2020, 1, 1);
-        df2 = new GregorianCalendar(2022, 1, 1);
-        mp = new MeetingImpl(1, dp, null);
-        mp2 = new MeetingImpl(2, dp2, null);
-        mf = new MeetingImpl(3, df, null);
-        mf2 = new MeetingImpl(4, df2, null);
-        meetings.put(1, mp);
-        meetings.put(2, mp2);
-        meetings.put(3, mf);
-        meetings.put(4, mf2);
+        participants = IntStream.range(1, 5)
+                .boxed()
+                .map(id -> new ContactImpl(id, "", ""))
+                .collect(Collectors.toSet());
+
+        cm = new ContactManagerImpl(meetings, contacts);
     }
 
     @Test
-    public void shouldReturnPastMeetingByID() throws Exception {
-        int id = 1;
-        int id2 = 2;
-        PastMeeting result = cm.getPastMeeting(id);
-        PastMeeting result2 = cm.getPastMeeting(id2);
-        assertThat(result.getDate(), is(dp));
-        assertThat(result.getId(), is(1));
-        assertThat(result2.getDate(), is(dp2));
-        assertThat(result2.getId(), is(2));
+    public void shouldAddFutureMeeting() throws Exception {
+        Calendar today = Calendar.getInstance(Locale.UK);
+        int meetingId = cm.addFutureMeeting(participants, today);
+        assertThat(meetingId, is(1));
     }
 }
