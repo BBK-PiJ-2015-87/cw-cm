88285bbe60d775a493d72d745ce0ceec1db94755
Vladimirs Ivanovs <ivanovs223@gmail.com>
2016-03-14 23:17:13 +0000

    Cleaned predicates

--- src/main/java/utils/ContactManagerPredicates.java
+++ src/main/java/utils/ContactManagerPredicates.java
@@ -14,76 +14,38 @@ import java.util.function.Predicate;
  * Created by vladimirsivanovs on 11/03/2016.
  */
 public class ContactManagerPredicates {
-    private final static long TIME_THRESHOLD = 5000;
     /**
      * Dummy predicate.
-     * @return true
+     * @return always true
      */
     public static Predicate<Meeting> dummyPredicate() {
         return meeting -> true;
     }
 
-    /**
-     * Tests if meeting is a future meeting
-     *
-     * @param date to compare with
-     * @return true if meeting is in future and of FutureMeeting type, false otherwise
-     */
     public static Predicate<Meeting> isFutureMeeting(Calendar date) {
         return meeting -> meeting.getDate().after(date) && meeting instanceof FutureMeeting;
     }
 
-    /**
-     * Tests if meeting is a past meeting
-     *
-     * @param date to compare with
-     * @return true if meeting is in the past and of PastMeeting type, false otherwise
-     */
     public static Predicate<Meeting> isPastMeeting(Calendar date) {
         return meeting -> meeting.getDate().before(date) && meeting instanceof PastMeeting;
     }
 
-    /**
-     *
-     * @param date
-     * @return
-     */
     public static Predicate<Meeting> meetingOn(Calendar date) {
         return meeting -> meeting.getDate().equals(date);
     }
 
-    /**
-     *
-     * @param id
-     * @return
-     */
     public static Predicate<Meeting> meetingWithID(int id) {
         return meeting -> meeting.getId() == id;
     }
 
-    /**
-     *
-     * @param contact
-     * @return
-     */
     public static Predicate<Meeting> meetingWithContact(Contact contact) {
         return meeting -> meeting.getContacts().contains(contact);
     }
 
-    /**
-     *
-     * @param name
-     * @return
-     */
     public static Predicate<Contact> contactWithName(String name) {
         return contact -> contact.getName().contains(name);
     }
 
-    /**
-     *
-     * @param ids
-     * @return
-     */
     public static Predicate<Contact> contactWithID(List<Integer> ids) {
         return contact -> ids.contains(contact.getId());
     }
